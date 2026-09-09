e5fef6c88c19e8df2d86ad991fef4719bcb4c2fa
Luis Ceja <lcejaqui@gmail.com>
2026-09-09 09:19:43 -0700

    Finished first iteration. May add a pause between dots to make it look like thinking and add some extra space for clear.

--- magic8ball.c
+++ magic8ball.c
@@ -1,14 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 #define BUFFER_SIZE 120
+
+char *responses[] = {
+    "Most undoubtably.", "It is written.", "Yes.", "Most assurably.",
+    "Indeed.","Absolutely.", "You may rely on it.", "Most likely.",
+    "This can never be.", "Unfortunately not.", "The gods will against this.", 
+    "No.", "Absolutely not.", "Probably... not.", "I doubt it.", "Don't rely on it.",
+    "This... I cannot say.","Unclear.","Try again, later.", "..." 
+};
+
+int f()
+{
+    return rand() % 19;
+}
+
 int main()
 {
     char *question = NULL;
-    ssize_t max = BUFFER_SIZE;
+    size_t max = BUFFER_SIZE;
+    srand(time(NULL));
+    int r = f();
+    printf("What does your heart desire to know?\n:");
     getline(&question, &max, stdin);
-    printf("Your question: %s\n", question);
 
+    printf("Visualizing the mind's eye...\n%s\n", responses[r] );
     free(question);
     return 0;
 }
+
+
